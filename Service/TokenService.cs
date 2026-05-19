86242079cd70edc4ceaaf5d8d671eade771bb0f1
feherszabolcs <feherszabi2005@gmail.com>
2026-05-19 20:36:57 +0200

    added id claim to jwt

--- Service/TokenService.cs
+++ Service/TokenService.cs
@@ -24,8 +24,9 @@ namespace apiv2.Service
         {
             var claims = new List<Claim>
             {
-                new(JwtRegisteredClaimNames.Email, user.Email),
-                new(JwtRegisteredClaimNames.GivenName, user.UserName)
+                new(JwtRegisteredClaimNames.Email, user.Email!),
+                new(JwtRegisteredClaimNames.GivenName, user.UserName!),
+                new(JwtRegisteredClaimNames.NameId, user.Id)
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
